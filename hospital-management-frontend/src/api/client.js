64ece718ef37878df4ad4ccdf6801fe3de734c5f
Dev Karan <dkvkaran2004@gmail.com>
2026-08-26 22:49:43 +0530

    changes secondtime

--- hospital-management-frontend/src/api/client.js
+++ hospital-management-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://medicare-hub-backend-ntpd.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
